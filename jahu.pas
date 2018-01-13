aa81a28e13fa6404847504b2686e763013bbbafa
alexandr <aleksandr-koryakov@mail.ru>
2018-01-13 23:05:55 +0300

    Simple programm

---
+++ jahu.pas
@@ -0,0 +1,37 @@
+﻿program Kirolor;//hjlfffkl
+
+
+var
+  bucket: array of integer := (1, 3, 4, 98, 56, 89);
+  condition: boolean;
+
+begin
+
+  var red: integer:= 89;
+  var fat: integer:= 67;
+  Println (red+' '+fat);
+  red:= red xor fat;
+  fat:= fat xor red;
+  red:= red xor fat;
+ Println (red+' '+fat); 
+  
+  ///condition := true;
+  //<less then
+  //>greater then
+  //==equal
+  //<>=not equal
+  //<=less or equal
+  //>=greater or equal
+ (* if (6>5)or(7>9) then
+  begin
+    Println('Жопа');
+  end
+  else
+  begin
+    Println('Peaceда')
+ /// end
+  *)
+end.
+
+
+
