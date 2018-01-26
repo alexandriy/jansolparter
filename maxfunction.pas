7dd9c26a37206ee6732b3629c05062fbfb81c075
alexandr <aleksandr-koryakov@mail.ru>
2018-01-26 23:32:48 +0300

    slau

---
+++ maxfunction.pas
@@ -0,0 +1,38 @@
+﻿program maxfunction;
+function max (a,b,c:real):real;
+begin
+
+  if (a>b) then
+    begin
+    //a greater than b/
+    if (a>c) then
+      begin
+        //a greater than c/
+      result:=a;
+      end
+      else
+      begin
+          //c greater than a/
+       result:=c;
+      end
+    end
+  else
+  //b greater than a/ 
+    begin
+  if (b>c) then 
+    begin
+      result:=b;
+    end
+   else
+    begin
+      result:=c;
+    end
+    
+    end;
+end;
+begin //main/
+var maxvalue: real;
+maxvalue:=max (50,71,100);
+Println (maxvalue);
+
+end.
---
+++ slau.pas
@@ -0,0 +1,39 @@
+﻿Program slau;
+// функция
+function solve(a,b,c,d,e,f:real):string;
+begin
+    // ax + by = c
+    // dx + ey = f
+    // det = |a b| |c|
+    //       |d e| |f| 
+    
+    // detX = |c b| 
+    //        |f e|  
+    
+    // detY = |a c|
+    //        |d f| 
+    
+    var det:real := a*e-b*d;
+    
+    // если det = 0
+    // A = B       
+    // |A-B| < Epsilon
+    if (abs(det) < real.Epsilon) then
+    begin
+       result := 'no solution';
+       exit;
+    end;
+    
+    var det_x:real := c*e-f*b;
+    var det_y:real := a*f-c*d;
+    var x:real := det_x/det;
+    var y:real := det_y/det;
+    
+    result := 'x=' + x + ' y=' + y;
+end;
+
+// главная программа
+begin
+   writeln(solve(1,1,3,1,-1,1));
+   writeln(solve(2,1,4,6,3,5));
+end.
